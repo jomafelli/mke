ed3bad67f89bfe79162efa41d665b50890f5c5b3
Markus M <jomafelli@gmail.com>
2021-06-11 12:54:01 +0200

    Inventory edited

--- server/src/main/java/de/llggiessen/mke/schema/Inventory.java
+++ server/src/main/java/de/llggiessen/mke/schema/Inventory.java
@@ -1,4 +1,22 @@
 package de.llggiessen.mke.schema;
 
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Year;
+
+@Entity
 public class Inventory {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ForeignKey
+    private Long id;
+    private Year year;
+    private String status;
+
+    @ForeignKey
+    private
 }
--- server/src/main/java/de/llggiessen/mke/schema/User.java
+++ server/src/main/java/de/llggiessen/mke/schema/User.java
@@ -88,7 +88,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" + "id=" + id + ", firstName='" + firstName + "\'" + ", lastName=" + lastName + "\'" + ", email='"
-                + email + "\'" + ", password=" + password + "\'" + "}";
+        return "User{" + "id=" + id + ", firstName='" + firstName + "'" + ", lastName=" + lastName + "'" + ", email='"
+                + email + "'" + ", password=" + password + "'" + "}";
     }
 }
