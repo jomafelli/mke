6f1cc38f7f1949d9d5b753fdd64a0b6d12ba9c38
ahmfaraz4 <faraz.ahmad@llg-giessen.de>
2021-06-16 06:12:00 +0000

    implementing @GetMapping in class InventoryController

--- server/src/main/java/de/llggiessen/mke/controller/InventoryController.java
+++ server/src/main/java/de/llggiessen/mke/controller/InventoryController.java
@@ -1,4 +1,18 @@
 package de.llggiessen.mke.controller;
 
+import de.llggiessen.mke.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/inventory")
 public class InventoryController {
+    @Autowired
+    InventoryRepository repository;
+
+    @GetMapping("")
+    public Iterable<user>
+    repository.findAllByAttributes()
 }
