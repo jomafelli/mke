4cc4c2be572cc203781b6d97069fb24bd8e776ae
ahmfaraz4 <faraz.ahmad@llg-giessen.de>
2021-07-14 07:24:39 +0000

    SQL GoQuestions done

--- server/src/main/java/de/llggiessen/mke/repository/SpaceUtilisationFieldRepository.java
+++ server/src/main/java/de/llggiessen/mke/repository/SpaceUtilisationFieldRepository.java
@@ -1,6 +1,12 @@
 package de.llggiessen.mke.repository;
 
+import de.llggiessen.mke.schema.Inventory;
 import de.llggiessen.mke.schema.SpaceUtilisationField;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SpaceUtilisationFieldRepository extends InventoryRepository<SpaceUtilisationField> {
+    @Query(value = "SELECT * FROM SpaceUtilisationField WHERE SpaceUtilisationField.type = :type", nativeQuery = true)
+    Iterable<SpaceUtilisationField> findAllByType(@Param("type") String spaceUtilisationField);
 }
